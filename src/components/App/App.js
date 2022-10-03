aea1adb75d3973f5270b2063a5f902eb604951d0
Pavel Tkachuk <p.s.tkachuk@yandex.ru>
2022-10-04 02:08:17 +0300

    сверстать footer

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import './App.css';
 import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
 
 function App() {
   const [loggedIn, setLoggedIn] = useState(false);
@@ -8,6 +9,7 @@ function App() {
   return (
     <div className="page">
       <Header loggedIn={ loggedIn } />
+      <Footer />
     </div>
   );
 }
