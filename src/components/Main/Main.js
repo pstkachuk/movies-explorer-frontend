7dca74b42b87088c5ed40fcf567d0c1ecf0ee11c
Pavel Tkachuk <p.s.tkachuk@yandex.ru>
2022-10-06 16:03:01 +0300

    сверстать Techs

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -2,6 +2,7 @@ import  './Main.css';
 import Promo from '../Promo/Promo';
 import NavTab from '../NavTab/NavTab';
 import AboutProject from '../AboutProject/AboutProject';
+import Techs from '../Techs/Techs';
 
 function Main() {
 
@@ -10,6 +11,7 @@ function Main() {
       <Promo />
       <NavTab />
       <AboutProject />
+      <Techs />
     </div>
   )
 }
