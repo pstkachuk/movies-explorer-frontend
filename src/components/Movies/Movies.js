abfc77393e65e620142ad731f5980cdfeb64f68f
Pavel Tkachuk <p.s.tkachuk@yandex.ru>
2022-10-10 18:21:52 +0300

    сверстать Preloader

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -1,12 +1,14 @@
 import './Movies.css';
 import SearchForm from '../SearchForm/SearchForm';
 import MoviesCardList from '../MoviesCardList/MoviesCardList';
+import Preloader from '../Preloader/Preloader';
 
 function Movies() {
   return (
     <div className="movies">
       <SearchForm />
       <MoviesCardList />
+      {/* <Preloader /> */}
     </div>
   )
 }
